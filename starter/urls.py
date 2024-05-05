e088ec8f65655f3519b8a9235f8a7bcdf1ddc680
codelab-kr <master@code-lab.kr>
2024-05-05 22:32:15 +0900

    remove playground

--- starter/urls.py
+++ starter/urls.py
@@ -25,7 +25,6 @@ from . import views
 urlpatterns = [
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
-    path("playground/", include("playground.urls")),
     path("admin/", admin.site.urls),
     path("", views.homepage),
     path("about/", views.about),
