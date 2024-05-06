f6188d834059fd1771a7d9cf6dc062e463cf4a29
codelab-kr <master@code-lab.kr>
2024-05-06 10:24:15 +0900

    add todo

--- todos/urls.py
+++ todos/urls.py
@@ -6,4 +6,8 @@ app_name = "todos"
 
 urlpatterns = [
     path("", views.todos_list, name="list"),
+    path("add/", views.add_todo, name="add"),
+    path("edit/<int:pk>/", views.edit_todo, name="edit"),
+    path("update/<int:pk>/", views.update_todo, name="update"),
+    path("delete/<int:pk>/", views.delete_todo, name="delete"),
 ]
