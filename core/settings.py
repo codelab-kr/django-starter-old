8e367ef7794a653eac06a7750c0ddd4232f95997
codelab-kr <master@code-lab.kr>
2024-05-09 13:41:40 +0900

    change lint for html

--- core/settings.py
+++ core/settings.py
@@ -107,7 +107,10 @@ DATABASES = {
     }
 }
 
-DATABASES["default"] = dj_database_url.parse(env("DATABASE_URL"))
+POSTGRES_RDS = env("POSTGRES_RDS", default=True)
+if ENVIRONMENT == "production" or POSTGRES_RDS:
+    DATABASES["default"] = dj_database_url.parse(env("DATABASE_URL"))
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
