efc50cc520ffae2f10bb82f36dbe68998d16b36f
codelab-kr <master@code-lab.kr>
2024-05-10 13:26:24 +0900

    apply cdk & cicd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['../**/templates/**/*.html', '../**/templates/*.html', '../**/forms.py'],
+  content: ['./**/templates/**/*.html', './**/templates/*.html', './**/forms.py'],
   theme: {
     extend: {},
   },
